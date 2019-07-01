c5b777ded59239cfd0160404e54d5442d45ef276
Maurits Meester <maurits@e-mmer.nl>
2019-07-01 22:45:20 +0200

    Connected to jira search to find origin ID

--- index.php
+++ index.php
@@ -10,14 +10,26 @@ $project_id = 21322146; // MIXED TEAM project
 
 $entries = $harvest->timeEntry()->list(['project_id'=>$project_id]);
 
+$headers = array( 'Accept' => 'application/json' );
+
+// basic auth
+\Unirest\Request::auth( $_SERVER['ATLASSIAN_USER'], $_SERVER['ATLASSIAN_TOKEN'] );
+
+
 foreach($entries as $entry){
 	preg_match('/^(GAZELLEB2C-\d+|BBB-\d+|FOCUS-\d+|KAL-\d+|CDB2BGAZ-\d+|MT-\d+)*$/', $entry->notes, $matches);
 	if($matches){ 
 		$matches['note'] = $entry->notes;
 		$matches['time'] = $entry->hours;
-		echo "<pre>";
-			print_r($matches);
-		echo "</pre>";
+		
+		$response = Unirest\Request::get(
+			'https://'.$_SERVER['JIRA_INSTANCE'].'.atlassian.net/rest/api/3/search?fields=project,issuetype,timeestimate,timeoriginalestimate,timetracking,summary,io.tempo.jira__team,io.tempo.jira__account&jql=issue in ("'.$matches[0].'")',
+			$headers
+		);
+		$jiraId = $response->body->issues[0]->id;
+		var_dump($jiraId);
+		break;
+		
 	}
 }
 
--- tempoConnect.php
+++ tempoConnect.php
@@ -5,6 +5,7 @@ require __DIR__ . '/globals.php';
 if(!isset($_GET['code'])) : ?>
 	<a href="https://<?= $_SERVER['JIRA_INSTANCE']; ?>.atlassian.net/plugins/servlet/ac/io.tempo.jira/oauth-authorize/?client_id=<?= $_SERVER['TEMPO_CLIENT_ID']; ?>&redirect_uri=<?= $_SERVER['TEMPO_REDIRECT']; ?>&access_type=tenant_user">Login to tempo</a>
 <?php else: 
+	// GIFLENS-https://media2.giphy.com/media/iPONcpDn3Rdss/200.gif
 	$handle = curl_init();
   curl_setopt($handle, CURLOPT_URL,"https://api.tempo.io/oauth/token/?grant_type=authorization_code&client_id=".$_SERVER['TEMPO_CLIENT_ID']."&client_secret=".$_SERVER['TEMPO_CLIENT_SECRET']."&redirect_uri=".$_SERVER['TEMPO_REDIRECT']."&code=".$_GET['code']);
 	curl_setopt($handle, CURLOPT_POST, 1);
