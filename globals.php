6c7562d0a38b3909e18104bee7a9293c2f6282b7
Maurits Meester <maurits@e-mmer.nl>
2019-07-02 20:08:56 +0200

    Add account mapping and extra env variables

--- globals.php
+++ globals.php
@@ -2,3 +2,5 @@
 
 $_SERVER['tokenPath'] = __DIR__ . '/token.json';
 $_SERVER['tempoBase'] = 'https://api.tempo.io';
+$_SERVER['JIRA_TICKETS'] = '/^(GAZELLEB2C-\d+|BBB-\d+|FOCUS-\d+|KAL-\d+|CDB2BGAZ-\d+|MT-\d+)*$/';
+$_SERVER['ACCOUNT_MAPPING'] = ['BBB' => 'BBB-ENH-B2C', 'FOCUS' => 'FOC-ENH-B2C', 'KAL' => 'KAL-ENH-B2C', 'MT' => 'MIX-MAIN-B2C'];
