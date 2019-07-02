1a4ae8c01cbee5e9afd4cecda91512c97716c871
Maurits Meester <maurits@e-mmer.nl>
2019-07-02 08:53:04 +0200

    Curl requests to unirest library

--- refreshToken.php
+++ refreshToken.php
@@ -1,19 +1,24 @@
 <?php 
 
+require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/globals.php';
 
 if (file_exists($_SERVER['tokenPath'])) {
 	$tokens = json_decode(file_get_contents($_SERVER['tokenPath']), true);
-	
-	$handle = curl_init();
-  curl_setopt($handle, CURLOPT_URL,"https://api.tempo.io/oauth/token/?grant_type=refresh_token&client_id=".$_SERVER['TEMPO_CLIENT_ID']."&client_secret=".$_SERVER['TEMPO_CLIENT_SECRET']."&redirect_uri=".$_SERVER['TEMPO_REDIRECT']."&refresh_token=".$tokens['refresh_token']);
-	curl_setopt($handle, CURLOPT_POST, 1);
-  curl_setopt($handle, CURLOPT_RETURNTRANSFER, 1);
-  $response = curl_exec($handle);
+  
+  
+  $headers = array('Accept' => 'application/json');
+  $query = array(
+              'grant_type' => 'refresh_token',
+              'client_id' => $_SERVER['TEMPO_CLIENT_ID'], 
+              'client_secret' => $_SERVER['TEMPO_CLIENT_SECRET'],
+              'redirect_uri' => $_SERVER['TEMPO_REDIRECT'],
+              'refresh_token' => $tokens['refresh_token'] );
+  $response = Unirest\Request::post('https://api.tempo.io/oauth/token/', $headers, $query);
 	
 	// Save the token to a file.
 	if (!file_exists(dirname($_SERVER['tokenPath']))) {
 			mkdir(dirname($_SERVER['tokenPath']), 0700, true);
 	}
-	file_put_contents($_SERVER['tokenPath'], $response);
+	file_put_contents($_SERVER['tokenPath'], $response->raw_body);
 }
--- tempoConnect.php
+++ tempoConnect.php
@@ -1,21 +1,24 @@
 <?php 
 
+require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/globals.php';
 
 if(!isset($_GET['code'])) : ?>
-	<a href="https://<?= $_SERVER['JIRA_INSTANCE']; ?>.atlassian.net/plugins/servlet/ac/io.tempo.jira/oauth-authorize/?client_id=<?= $_SERVER['TEMPO_CLIENT_ID']; ?>&redirect_uri=<?= $_SERVER['TEMPO_REDIRECT']; ?>&access_type=tenant_user">Login to tempo</a>
+  <a href="https://<?= $_SERVER['JIRA_INSTANCE']; ?>.atlassian.net/plugins/servlet/ac/io.tempo.jira/oauth-authorize/?client_id=<?= $_SERVER['TEMPO_CLIENT_ID']; ?>&redirect_uri=<?= $_SERVER['TEMPO_REDIRECT']; ?>&access_type=tenant_user">Login to tempo</a>
 <?php else: 
-	// GIFLENS-https://media2.giphy.com/media/iPONcpDn3Rdss/200.gif
-	$handle = curl_init();
-  curl_setopt($handle, CURLOPT_URL,"https://api.tempo.io/oauth/token/?grant_type=authorization_code&client_id=".$_SERVER['TEMPO_CLIENT_ID']."&client_secret=".$_SERVER['TEMPO_CLIENT_SECRET']."&redirect_uri=".$_SERVER['TEMPO_REDIRECT']."&code=".$_GET['code']);
-	curl_setopt($handle, CURLOPT_POST, 1);
-  curl_setopt($handle, CURLOPT_RETURNTRANSFER, 1);
-  $response = curl_exec($handle);
-		
-	
-	// Save the token to a file.
-	if (!file_exists(dirname($_SERVER['tokenPath']))) {
-			mkdir(dirname($_SERVER['tokenPath']), 0700, true);
-	}
-	file_put_contents($_SERVER['tokenPath'], $response);
+  $headers = array('Accept' => 'application/json');
+  $query = array(
+              'grant_type' => 'authorization_code',
+              'client_id' => $_SERVER['TEMPO_CLIENT_ID'], 
+              'client_secret' => $_SERVER['TEMPO_CLIENT_SECRET'],
+              'redirect_uri' => $_SERVER['TEMPO_REDIRECT'],
+              'code' => $_GET['code'] );
+
+  $response = Unirest\Request::post('https://api.tempo.io/oauth/token/', $headers, $query);
+  
+  // Save the token to a file.
+  if (!file_exists(dirname($_SERVER['tokenPath']))) {
+      mkdir(dirname($_SERVER['tokenPath']), 0700, true);
+  }
+  file_put_contents($_SERVER['tokenPath'], $response->raw_body);
 endif; ?>
